5b3ddb7fc87be7d62905368cacb34734357e36ca
hisosa <hersonsosa1@gmail.com>
2022-03-15 14:01:08 -0400

    added controller

--- backend-api/src/main/java/org/wcci/apimastery/Controller/AlbumController.java
+++ backend-api/src/main/java/org/wcci/apimastery/Controller/AlbumController.java
@@ -1,8 +1,19 @@
 package org.wcci.apimastery.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.wcci.apimastery.Repositories.AlbumRepository;
+import org.wcci.apimastery.Repositories.SongRepository;
 
-@Controller
+@RestController
 public class AlbumController {
+
+    private AlbumRepository albumRepo;
+    private SongRepository songRepo;
+
+    public AlbumController(AlbumRepository albumRepo, SongRepository songRepo){
+        this.albumRepo = albumRepo;
+        this.songRepo = songRepo;
+    }
 
 }
