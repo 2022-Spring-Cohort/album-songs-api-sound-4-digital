210c6d0f61e9081504d16b531d7eb3ffd1a9e87c
hisosa <hersonsosa1@gmail.com>
2022-03-15 15:05:31 -0400

    populator started

--- backend-api/src/main/java/org/wcci/apimastery/Populator.java
+++ backend-api/src/main/java/org/wcci/apimastery/Populator.java
@@ -1,10 +1,30 @@
 package org.wcci.apimastery;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.wcci.apimastery.Entity.Album;
+import org.wcci.apimastery.Entity.Song;
+import org.wcci.apimastery.Repositories.AlbumRepository;
+import org.wcci.apimastery.Repositories.SongRepository;
+
+@Component
 public class Populator implements CommandLineRunner {
+    @Autowired
+    private AlbumRepository albumRepo;
+    @Autowired
+    private SongRepository songRepo;
 
     @Override
     public void run(String... args) throws Exception {
 
     }
+
+//    @Override
+//    public void run(String... args) throws Exception {
+//        Album album1 = new Album("title","image","recordLabel");
+//        albumRepo.save(album1);
+//        Song song1 = new Song("track01","https", 12,);
+//        songRepo.save(song1);
+//    }
 }
