a1f125ed322ca43625bbd0ca6d3144bc28067336
mccurdyr <ryan.mccurdy96@gmail.com>
2022-03-24 11:51:23 -0400

    got the addNewAlbumBtn working

--- frontend-spa/src/js/albumsJSON.js
+++ frontend-spa/src/js/albumsJSON.js
@@ -1,174 +1,212 @@
 const albums =
-  [
-    {
-      "id": 1,
-      "title": "I'm With You",
-      "image": "img_url",
-      "recordLabel": "Warner Records",
-      "songs": [
-        {
-          "id": 6,
-          "songTitle": "",
-          "duration": "5:20",
-          "ratings": 5,
-          "comments": [
-            "great song"
-          ]
-        },
-        {
-          "id": 12,
-          "songTitle": "Ethiopia",
-          "duration": "4:44",
-          "ratings": 7,
-          "comments": [
-            "life changing song"
-          ]
-        },
-        {
-          "id": 13,
-          "songTitle": "Happiness Loves Company",
-          "duration": "6:66",
-          "ratings": 6,
-          "comments": [
-            "diabolical"
-          ]
-        }
-      ],
-      "comments": [
-        "it rocks"
-      ],
-      "ratings": 5
-    },
-    {
-      "id": 2,
-      "title": "Take Care",
-      "image": "img",
-      "recordLabel": "sd",
-      "songs": [
-        {
-          "id": 9,
-          "songTitle": "Make me proud",
-          "duration": "4:20",
-          "ratings": 4,
-          "comments": [
-            "dope"
-          ]
-        },
-        {
-          "id": 10,
-          "songTitle": "Practice",
-          "duration": "4:17",
-          "ratings": 3,
-          "comments": [
-            "lmao"
-          ]
-        }
-      ],
-      "comments": [
-        "it's the worst"
-      ],
-      "ratings": 80
-    },
-    {
-      "id": 3,
-      "title": "Star Boy",
-      "image": "img_url2",
-      "recordLabel": "Best One",
-      "songs": [
-        {
-          "id": 8,
-          "songTitle": "title of song",
-          "duration": "2:37",
-          "ratings": 5,
-          "comments": [
-            "popular"
-          ]
-        },
-        {
-          "id": 11,
-          "songTitle": "Coca-Cola",
-          "duration": "2:45",
-          "ratings": 3,
-          "comments": [
-            "great song"
-          ]
-        }
-      ],
-      "comments": [
-        "Awesome"
-      ],
-      "ratings": 10
-    },
-    {
-      "id": 4,
-      "title": "Headlines",
-      "image": "img",
-      "recordLabel": "ys",
-      "songs": [
-        {
-          "id": 7,
-          "songTitle": "this is a title",
-          "duration": "7:00",
-          "ratings": 5,
-          "comments": [
-            "excellent"
-          ]
-        },
-        {
-          "id": 16,
-          "songTitle": "Fleas",
-          "duration": "3:00",
-          "ratings": 8,
-          "comments": [
-            "dope stuff"
-          ]
-        }
-      ],
-      "comments": [
-        "great taste"
-      ],
-      "ratings": 9
-    },
-    {
-      "id": 5,
-      "title": "Punk in Drublic",
-      "image": "",
-      "recordLabel": "Self-published",
-      "songs": [
-        {
-          "id": 14,
-          "songTitle": "Linoleum",
-          "duration": "4:00",
-          "ratings": 6,
-          "comments": [
-            "cuh-razy"
-          ]
-        },
-        {
-          "id": 15,
-          "songTitle": "The Cause",
-          "duration": "7:50",
-          "ratings": 7,
-          "comments": [
-            "too quiet"
-          ]
-        },
-        {
-          "id": 17,
-          "songTitle": "Jeff Wears Birkenstocks",
-          "duration": "4:10",
-          "ratings": 4,
-          "comments": [
-            "hello-world"
-          ]
-        }
-      ],
-      "comments": [
-        ""
-      ],
-      "ratings": 10
-    }
-  ]
+[
+  {
+    "id": 1,
+    "title": "I'm With You",
+    "image": "./src/img/withYou.jpg",
+    "recordLabel": "Warner Records",
+    "songs": [
+      {
+        "id": 7,
+        "songTitle": "Goodbye Hooray",
+        "duration": "5:20",
+        "comments": [
+          "great song"
+        ],
+        "songRatings": 5.0
+      },
+      {
+        "id": 8,
+        "songTitle": "Even You, Brutus?",
+        "duration": "7:00",
+        "comments": [
+          "excellent"
+        ],
+        "songRatings": 5.0
+      },
+      {
+        "id": 13,
+        "songTitle": "Ethiopia",
+        "duration": "4:44",
+        "comments": [
+          "life changing song"
+        ],
+        "songRatings": 7.0
+      },
+      {
+        "id": 14,
+        "songTitle": "Happiness Loves Company",
+        "duration": "6:66",
+        "comments": [
+          "diabolical"
+        ],
+        "songRatings": 6.0
+      }
+    ],
+    "comments": [
+      "it rocks"
+    ],
+    "albumRating": 5.0
+  },
+  {
+    "id": 2,
+    "title": "Take Care",
+    "image": "./src/img/takeCare.jpg",
+    "recordLabel": "sd",
+    "songs": [
+      {
+        "id": 10,
+        "songTitle": "Make me proud",
+        "duration": "4:20",
+        "comments": [
+          "dope"
+        ],
+        "songRatings": 4.0
+      },
+      {
+        "id": 11,
+        "songTitle": "Practice",
+        "duration": "4:17",
+        "comments": [
+          "lmao"
+        ],
+        "songRatings": 3.0
+      }
+    ],
+    "comments": [
+      "it's the worst"
+    ],
+    "albumRating": 80.0
+  },
+  {
+    "id": 3,
+    "title": "Star Boy",
+    "image": "./src/img/starboy.png",
+    "recordLabel": "Best One",
+    "songs": [
+      {
+        "id": 9,
+        "songTitle": "African",
+        "duration": "2:37",
+        "comments": [
+          "popular"
+        ],
+        "songRatings": 5.0
+      },
+      {
+        "id": 12,
+        "songTitle": "Coca-Cola",
+        "duration": "2:45",
+        "comments": [
+          "great song"
+        ],
+        "songRatings": 3.0
+      }
+    ],
+    "comments": [
+      "Awesome"
+    ],
+    "albumRating": 10.0
+  },
+  {
+    "id": 4,
+    "title": "Nothing Was The Same",
+    "image": "./src/img/nwts.jpg",
+    "recordLabel": "ys",
+    "songs": [
+      {
+        "id": 19,
+        "songTitle": "Tuscan Leather",
+        "duration": "6:06",
+        "comments": [
+          "Banger of an intro"
+        ],
+        "songRatings": 5.0
+      },
+      {
+        "id": 20,
+        "songTitle": "Started From the Bottom",
+        "duration": "2:54",
+        "comments": [
+          "meme"
+        ],
+        "songRatings": 5.0
+      },
+      {
+        "id": 21,
+        "songTitle": "Own It",
+        "duration": "4:12",
+        "comments": [
+          ""
+        ],
+        "songRatings": 5.0
+      }
+    ],
+    "comments": [
+      "great taste"
+    ],
+    "albumRating": 9.0
+  },
+  {
+    "id": 5,
+    "title": "Punk in Drublic",
+    "image": "./src/img/punk.jpg",
+    "recordLabel": "Self-published",
+    "songs": [
+      {
+        "id": 15,
+        "songTitle": "Linoleum",
+        "duration": "4:00",
+        "comments": [
+          "cuh-razy"
+        ],
+        "songRatings": 6.0
+      },
+      {
+        "id": 16,
+        "songTitle": "The Cause",
+        "duration": "7:50",
+        "comments": [
+          "too quiet"
+        ],
+        "songRatings": 7.0
+      },
+      {
+        "id": 17,
+        "songTitle": "Fleas",
+        "duration": "3:00",
+        "comments": [
+          "dope stuff"
+        ],
+        "songRatings": 8.0
+      },
+      {
+        "id": 18,
+        "songTitle": "Jeff Wears Birkenstocks",
+        "duration": "4:10",
+        "comments": [
+          "hello-world"
+        ],
+        "songRatings": 4.0
+      }
+    ],
+    "comments": [
+      ""
+    ],
+    "albumRating": 10.0
+  },
+  {
+    "id": 6,
+    "title": "Always & Forever",
+    "image": "heart.jpg",
+    "recordLabel": "heart-eyes",
+    "songs": [],
+    "comments": [
+      "ILY"
+    ],
+    "albumRating": 100.0
+  }
+]
 export {
   albums
 }
